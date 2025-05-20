5b868e685f9a75c1d4a1b1fd252c2a822223a9ae
Barbora Srenkova <bara.srenkova@gmail.com>
2025-05-20 10:46:12 +0100

    Initial commit

---
+++ src/test/java/TodoMVCProjectTest.java
@@ -0,0 +1,55 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.io.File;
+import java.time.Duration;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TodoMVCTesting {
+    private static ChromeDriver driver;
+
+    @BeforeAll
+    static void launchBrowser() {
+        driver = new ChromeDriver();
+    }
+    @Test{
+    public static void main(String[] args) throws Exception{
+
+        // Use WebDriver to open a new instance of ChromeDriver
+        WebDriver driver = new ChromeDriver();
+
+        // Instruct the driver to browse to the Makers website
+        driver.get("https://makers.tech");
+
+        // Take a screenshot of what's currently on the page,
+        // and store it in a file 'makers.png' in your project root
+        takeScreenshot(driver, "makers.png");
+
+        // Find the title of the webpage (the value inside the HTML
+        // <title> element) and print it to the terminal
+        System.out.println(driver.getTitle());
+
+        // Close down Selenium and end the test
+        driver.quit();
+    }
+
+    // Helper function for taking screenshots using WebDriver
+    public static void takeScreenshot(WebDriver webdriver,String desiredPath) throws Exception{
+        TakesScreenshot screenshot = ((TakesScreenshot)webdriver);
+        File screenshotFile = screenshot.getScreenshotAs(OutputType.FILE);
+        File targetFile = new File(desiredPath);
+        FileUtils.copyFile(screenshotFile, targetFile);
+    }
+}}
+    @AfterAll
+        static void closeBrowser() {
+        driver.quit();
+    }
+
