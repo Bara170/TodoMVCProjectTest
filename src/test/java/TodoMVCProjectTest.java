89fc5b7211b70befaf37aa8625898a9991dca662
Barbora Srenkova <bara.srenkova@gmail.com>
2025-05-20 11:20:38 +0100

    Initial commit

--- src/test/java/TodoMVCProjectTest.java
+++ src/test/java/TodoMVCProjectTest.java
@@ -1,45 +1,40 @@
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 import java.io.File;
-import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-// THIS IS A TEST THIS IS A TEST THIS IS A TEST
-
-public class TodoMVCTesting {
+public class TodoMVCProjectTest {
     private static ChromeDriver driver;
 
     @BeforeAll
     static void launchBrowser() {
         driver = new ChromeDriver();
     }
-    @Test{
-    public static void main(String[] args) throws Exception{
 
-        // Use WebDriver to open a new instance of ChromeDriver
+    @Test
+    void navigateToWebsite() throws Exception {
         WebDriver driver = new ChromeDriver();
 
-        // Instruct the driver to browse to the Makers website
-        driver.get("https://makers.tech");
+        // Instruct the driver to browse to the TODOMVC website
+        driver.get("https://todomvc.com/");
+
 
         // Take a screenshot of what's currently on the page,
-        // and store it in a file 'makers.png' in your project root
-        takeScreenshot(driver, "makers.png");
+        // and store it in a file 'initial_screenshot.png' in your project root
+        takeScreenshot(driver, "initial_screenshot.png");
 
         // Find the title of the webpage (the value inside the HTML
         // <title> element) and print it to the terminal
         System.out.println(driver.getTitle());
 
-        // Close down Selenium and end the test
-        driver.quit();
     }
 
     // Helper function for taking screenshots using WebDriver
@@ -49,9 +44,8 @@ public class TodoMVCTesting {
         File targetFile = new File(desiredPath);
         FileUtils.copyFile(screenshotFile, targetFile);
     }
-}}
     @AfterAll
         static void closeBrowser() {
         driver.quit();
-    }
+    }}
 
